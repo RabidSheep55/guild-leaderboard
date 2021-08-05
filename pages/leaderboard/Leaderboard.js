3ac5064568f54a46913cd6ef248fe0a112498605
RabidSheep55 <tharreaupierre@gmail.com>
2021-08-05 17:27:40 +0200

    return cacheInfo for debugging

--- pages/leaderboard/Leaderboard.js
+++ pages/leaderboard/Leaderboard.js
@@ -4,7 +4,9 @@ import fetch from "isomorphic-unfetch";
 
 import styles from "./styles/Leaderboard.module.css";
 
-const Leaderboard = ({ data }) => {
+const Leaderboard = ({ data, cacheInfo }) => {
+  console.log("Cache Info", cacheInfo);
+
   return (
     <div className={styles.mainContainer}>
       <h1 className={styles.title}>Guild Event Leaderboard</h1>
@@ -23,8 +25,15 @@ export async function getServerSideProps(context) {
   // Fetch from API
   const res = await fetch("http://localhost:3000/api/leaderboard");
   const json = await res.json();
+  const headers = res.headers;
 
   return {
-    props: { data: json },
+    props: {
+      data: json,
+      cacheInfo: {
+        "HIT or Miss": res.headers.get("X-Cache"),
+        Age: res.headers.get("X-Cache-Age-s"),
+      },
+    },
   };
 }

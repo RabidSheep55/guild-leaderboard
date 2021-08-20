06264ee2cc58eee607b1fd210756d02ea5150958
RabidSheep55 <tharreaupierre@gmail.com>
2021-08-21 00:02:08 +0200

    add no data warning

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -3,6 +3,7 @@ import TableItem from "./components/TableItem";
 import styles from "./styles/Table.module.css";
 
 const Table = ({ data }) => {
+  console.log(data);
   return (
     <div className={styles.container}>
       <div className={[styles.headers, styles.tableSettings].join(" ")}>
@@ -17,7 +18,7 @@ const Table = ({ data }) => {
           Points
         </div>
       </div>
-      {data &&
+      {data.length ? (
         data.map((item, index) => (
           <TableItem
             key={index}
@@ -28,7 +29,14 @@ const Table = ({ data }) => {
             darker={index % 2 === 0}
             last={index === data.length - 1}
           />
-        ))}
+        ))
+      ) : (
+        <div className={styles.nodata}>
+          <p>
+            <i>This event doesn&apos;t appear have player data yet :/</i>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
